b76ec2028a777bc8d4a8d290aa0c431b92ee3e86
Rivka Schuss <rschuss123@gmail.com>
2019-01-13 16:38:07 +0200

    finished knn

--- ex2_py.py
+++ ex2_py.py
@@ -8,6 +8,7 @@ def find_positive_tag(tags):
             return tag
     return tag[0]
 
+
 class DtlNode:
     def __init__(self, attr, depth, leaf=False, pred=None):
         self.attr = attr
@@ -54,19 +55,13 @@ class DtlClassifier:
         examples_merged_tags = [(example, tag) for example, tag in zip(self.examples, self.tags)]
         self.tree = DtlTree(self.DTL(examples_merged_tags, attributes, 0, self.get_mode(tags)), attributes)
 
-    def write_tree_to_file(self, output_file_name):
+    def write_to_file(self, output_file_name):
         with open(output_file_name, "w") as output:
             tree_string = self.tree.tree_string(self.tree.root)
             output.write(tree_string[:len(tree_string) - 1])
             
     def predict(self, ex):
         return self.tree.traversal(ex)
-
-    # def find_positive_tag(self, tags):
-    #     for tag in tags:
-    #         if tag in ["yes", "true"]:
-    #             return tag
-    #     return tag[0]
 
     def get_mode(self, tags):
         tags_counter = Counter()
@@ -79,18 +74,18 @@ class DtlClassifier:
         return tags_counter.most_common(1)[0][0]
 
     def calc_entropy(self, tags):
-        tags_counter = Counter()
+        tags_count = Counter()
+        entropy = 0
 
-        if not tags:
+        if not len(tags):
             return 0
 
         for tag in tags:
-            tags_counter[tag] += 1
-        class_prob = [tags_counter[tag] / float(len(tags)) for tag in tags_counter]
+            tags_count[tag] += 1
+        class_prob = [tags_count[tag] / float(len(tags)) for tag in tags_count]
         if 0.0 in class_prob:
             return 0
 
-        entropy = 0
         for prob in class_prob:
             entropy -= prob * math.log(prob, 2)
 
@@ -106,7 +101,7 @@ class DtlClassifier:
 
     def get_gain(self, examples, tags, attribute):
         initial_entropy = self.calc_entropy(tags)
-        relative_entropy_per_feature = []
+        relative_entropy_attr = []
         attr_index = self.attributes.index(attribute)
         for possible_value in self.att_dom_dict[attribute]:
             examples_and_tags_vi = [(example, tag) for example, tag in zip(examples, tags)
@@ -116,9 +111,9 @@ class DtlClassifier:
             if not examples:
                 pass
             relative_entropy = (float(len(examples_and_tags_vi)) / len(examples)) * entropy_vi
-            relative_entropy_per_feature.append(relative_entropy)
+            relative_entropy_attr.append(relative_entropy)
 
-        return initial_entropy - sum(relative_entropy_per_feature)
+        return initial_entropy - sum(relative_entropy_attr)
 
     def choose_attribute(self, attributes, examples, tags):
         gain_dict = {attribute: self.get_gain(examples, tags, attribute) for attribute in attributes}
@@ -134,8 +129,8 @@ class DtlClassifier:
         if not len(data):
             return DtlNode(None, depth, True, default)
 
-        tags = [tag for example, tag in data]
-        examples = [example for example, tag in data]
+        tags = [tag for ex, tag in data]
+        examples = [ex for ex, tag in data]
 
         if len(set(tags)) == 1:
             return DtlNode(None, depth, True, tags[0])
@@ -163,7 +158,6 @@ class KnnClassifer:
         self.k = k
 
     def calc_distance(self, ex1, ex2):
-
         distance = 0
         for attr1, attr2 in zip(ex1, ex2):
             if attr1 != attr2:
@@ -186,19 +180,37 @@ class KnnClassifer:
 
         closest_k = sorted(distances, key=lambda x: x[1])[:self.k]
 
-        closest_k = [item[0][1] for item in closest_k]
+        closest_k = [element[0][1] for element in closest_k]
         return self.get_common_tag(closest_k)
-
-
 
 
 class NaiveClassifier:
     def __init__(self, examples, tags):
         self.examples = examples
         self.tags = tags
+        self.examples_dict = self.get_examples_dict()
+        self.attr_dom_dict = self.get_attr_dom_dict()
+
+    def get_examples_dict(self):
+        examples_dict = {}
+        for ex, tag in zip(self.examples, self.tags):
+            if tag in examples_dict:
+                examples_dict[tag].append(ex)
+            else:
+                examples_dict[tag] = [ex]
+
+        return examples_dict
+
+    def get_attr_dom_dict(self):
+        attr_domain_size_dict = {}
+        for attr_index in range(len(self.examples[0])):
+            domain = set([ex[attr_index] for ex in self.examples])
+            attr_domain_size_dict[attr_index] = len(domain)
+        return attr_domain_size_dict
 
     def predict(self, ex):
         pass
+
 
 
 def read__file(file_name):
@@ -222,8 +234,7 @@ def write_files(test_tags, preds, accuracies, dtl_classifier):
     dt_preds, knn_preds, naive_preds = preds[0], preds[1], preds[2]
     dt_acc, knn_acc, naive_acc = accuracies[0], accuracies[1], accuracies[2]
     with open("output.txt", "w") as output:
-        lines = []
-        lines.append("Num\tDT\tKNN\tnaiveBayes")
+        lines = ["Num\tDT\tKNN\tnaiveBayes"]
         i = 1
         for true_tag, dt_pred, knn_pred, naive_pred in zip(test_tags, dt_preds, knn_preds, naive_preds):
             lines.append("{}\t{}\t{}\t{}".format(i, dt_pred, knn_pred, naive_pred))
@@ -231,7 +242,7 @@ def write_files(test_tags, preds, accuracies, dtl_classifier):
         lines.append("\t{}\t{}\t{}".format(dt_acc, knn_acc, naive_acc))
         output.writelines("\n".join(lines))
 
-    dtl_classifier.write_tree_to_file("output_tree.txt")
+    dtl_classifier.write_to_file("output_tree.txt")
 
 
 def get_accuracy(test_tags, preds):
@@ -248,7 +259,6 @@ def get_accuracy(test_tags, preds):
 if __name__ == '__main__':
     attributes, train_examples, train_tags = read__file("train.txt")
     place_holder, test_examples, test_tags = read__file("test.txt")
-    print(attributes, train_examples, train_tags)
 
     dtl_classifier = DtlClassifier(attributes[:len(attributes) - 1], train_examples, train_tags)
     knn_classifier = KnnClassifer(train_examples, train_tags)
